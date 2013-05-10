c4c26d74a84e069bf4316eef695fda0f2495fff8
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-05-10 22:26:08 +0200

    Add vagrant

---
+++ Vagrantfile
@@ -0,0 +1,9 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "precise32"
+  config.vm.box_url = "http://files.vagrantup.com/precise32.box"
+  config.vm.provision :shell, :path => "bootstrap.sh"
+  config.vm.network :forwarded_port, host: 4567, guest: 80
+end
---
+++ bootstrap.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# install all softwares
+
+apt-get update
+apt-get install -y nginx python-software-properties python g++ make
+
+if [ ! -e "/etc/apt/sources.list.d/chris-lea-node_js-precise.list" ]
+then
+	add-apt-repository ppa:chris-lea/node.js
+fi
+
+apt-get update
+apt-get install -y nodejs
+
+# run node apps
+
+cd /vagrant
+
+npm install
+
+cp /vagrant/init/* /etc/init.d
+
+/etc/init.d/prime start
+/etc/init.d/elements start
+
