39d373a583798e859b1d1ab0edd2a83e3429c51a
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-05-11 03:43:01 +0200

    Add main website, and use prime-website / elements-website as sub apps

---
+++ index.js
@@ -0,0 +1,31 @@
+
+var application = require('mootools-microsite');
+var express = app.express;
+
+var routes = function(app){
+
+	app.use(function setPath(req, res, next){
+		res.locals.path = app.path()
+		next();
+	});
+
+	app.get('/', function(req, res){
+		res.render('index', {
+			title: 'MooTools'
+		});
+	});
+
+};
+
+var statics = function(app){
+	app.use(express.static(__dirname + '/public'));
+}
+
+var app = application({
+	routes: routes,
+	static: statics,
+	dirname: __dirname
+});
+
+app.use('/prime', require('./prime-website'));
+app.use('/elements', require('./elements-website'));
