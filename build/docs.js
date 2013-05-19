9c8fce967f8eb974e0f77088c04ae96759cee8b9
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-05-19 15:17:16 +0200

    Move the compile markdown function in a separate module

--- build/docs.js
+++ build/docs.js
@@ -1,12 +1,10 @@
 "use strict";
 
-var fs = require('fs-extra');
+var fs = require('fs');
 var async = require('async');
 var path = require('path');
-var rs = require('robotskirt');
-var hljs = require('highlight.js');
-var slug = require ('slug');
 var semver = require('semver');
+var compile = require('../lib/compile-md');
 
 var args = process.argv;
 
@@ -73,40 +71,4 @@ function build(project, docsdir){
 		console.log("done building documentation html files for " + project);
 	});
 
-}
-
-function compile(md){
-
-	var renderer = new rs.HtmlRenderer();
-
-	renderer.blockcode = function(code, lang){
-		if (!lang) return '<pre><code>' + code + '</code></pre>\n';
-		if (lang == 'js') lang = 'javascript';
-		else if (lang == 'html') lang = 'xml';
-		else if (lang == 'shell') lang = 'bash';
-		code = hljs.highlight(lang, code).value.trim();
-		return '<pre><code class="' + lang + '">' + code + '</code></pre>\n';
-	};
-
-	var sidebar = '';
-	var links = {};
-	renderer.header = function(text, level){
-		if (level <= 2){
-
-			// handle duplicate headers
-			var link = slug(text);
-			if (links[link]) link += '-' + links[link]++;
-			else links[link] = 1;
-
-			sidebar += '<a href="#' + link + '"' + (level == 1 ? ' class="top"' : '') + '>' + text + '</a>\n';
-			text = '<a href="#' + link + '" name="' + link + '">' + text + '</a>';
-		}
-		return '<h' + level + '>' + text + '</h' + level + '>\n';
-	};
-
-	var parser = new rs.Markdown(renderer, [rs.EXT_FENCED_CODE]);
-
-	var html = parser.render(md);
-
-	return {content: html, toc: sidebar};
 }
