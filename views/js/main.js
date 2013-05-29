fc2fee79f02c8fe77ac9ec390a66fc25e23defb5
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-05-29 23:40:06 +0200

    Toggle the navigation on the project pages

--- views/js/main.js
+++ views/js/main.js
@@ -1,1 +1,25 @@
-console.log("wrapup works");
+"use strict";
+
+if (window.matchMedia){
+
+	// we can use all this, because matchMedia is for modern browser
+	// only anyway
+
+	window.addEventListener('DOMContentLoaded', function(){
+
+		var navigation = document.querySelector('#header ul');
+		var selected = document.querySelector('#header ul li.selected');
+
+		var opened = false;
+		selected.addEventListener('click', function(event){
+			var match = window.matchMedia('only screen and (max-width: 47.999em)');
+			if (match.matches){
+				event.preventDefault();
+				navigation.classList.toggle('opened');
+				opened = !opened;
+			}
+		}, false);
+
+	}, false);
+
+}
