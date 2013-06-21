bf6ec91747c7bcf83635e48d86a10b5723e2a000
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-06-21 23:13:35 +0200

    Also do initial stuff for the download builder in build/all

--- build/all.js
+++ build/all.js
@@ -4,21 +4,22 @@ var async = require('async');
 var spawn = require('child_process').spawn;
 
 var cmds = [
-	["docs", "prime"],
-	["docs", "elements"],
-	["docs", "moofx"],
-	["docs", "agent"],
-	["guides", "prime"],
-//	["guides", "elements"],
-	["guides", "moofx"],
-	["guides", "agent"],
-	["blog"]
+	["build/docs", "prime"],
+	["build/docs", "elements"],
+	["build/docs", "moofx"],
+	["build/docs", "agent"],
+	["build/guides", "prime"],
+//	["build/guides", "elements"],
+	["build/guides", "moofx"],
+	["build/guides", "agent"],
+	["build/blog"],
+	["node_modules/.bin/wrapup-webbuilder-init"]
 ];
 
 async.each(cmds, function(cmd, callback){
 	var node = spawn('node', cmd, {
 		stdio: 'inherit',
-		cwd: __dirname
+		cwd: __dirname + '/..'
 	});
 	node.on('close', callback);
 });
