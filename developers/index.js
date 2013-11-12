e91c5920e7d29e19d41b6113c5cdefb30c8cdc82
SergioCrisostomo <sergiosbox@gmail.com>
2013-11-12 11:08:03 +0000

    Added resort to developer list order

--- developers/index.js
+++ developers/index.js
@@ -3,6 +3,10 @@
 module.exports = function(app){
 	
 	var developersData     = require('./developers');
+	
+	var reSort = function(){return 0.5 - Math.random()};
+	developersData.developers = developersData.developers.sort(reSort());
+	developersData.alumni = developersData.alumni.sort(reSort());
 
 	var developers = function(req, res, next){
 		res.locals.site = 'developers';
