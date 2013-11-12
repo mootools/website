156d11237eb0302a7153943974568747e9511d31
SergioCrisostomo <sergiosbox@gmail.com>
2013-11-12 09:29:16 +0000

    Imported developers page from old site

--- developers/index.js
+++ developers/index.js
@@ -1,6 +1,8 @@
 "use strict";
 
 module.exports = function(app){
+	
+	var developersData     = require('./developers');
 
 	var developers = function(req, res, next){
 		res.locals.site = 'developers';
@@ -10,8 +12,38 @@ module.exports = function(app){
 	app.get('/developers', developers, function(req, res){
 		res.render('developers/index', {
 			page: "/developers",
-			title: "MooTools Developers"
+			title: "MooTools Developers",
+			developersData: developersData
 		});
 	});
 
 };
+
+
+/*
+"use strict";
+
+var express = require('express');
+var app = express();
+
+var developersData     = require('./developers');
+console.log(developersData);
+
+module.exports = function(app){
+
+	var developers = function(req, res, next){
+		res.locals.site = 'developers';
+		next();
+	};
+
+	app.get('/', function(req, res){
+
+	  // render the Jade template located in views/developers/index.jade file passing in data
+	  res.render('index', {developersData: data});
+
+	});
+	
+
+};
+
+*/
