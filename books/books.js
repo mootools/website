01c340c9e3560524736b8143cac3fff581eadd9d
Sergio Crisostomo <sergiosbox@gmail.com>
2014-10-13 09:34:25 +0200

    use alt tags
    use alt tags in image
    open books in current page

--- books/books.js
+++ books/books.js
@@ -3,19 +3,19 @@ var books = [
 		"title": "Pro JavaScript with MooTools",
 		"description": "Ready to take your MooTools knowledge to a new level? Then <a href=\"http://www.amazon.com/Pro-JavaScript-MooTools-Mark-Obcena/dp/1430230541/\">grab a copy</a> of <a href=\"http://projswithmootools.com/\">Pro JavaScript with MooTools</a> by <a href=\"http://keetology.com\">Mark Obcena</a>, with a Foreword by MooTools Founder and Lead Developer <a href=\"http://mad4milk.net\">Valerio Proietti</a>.</p><p>Geared towards intermediate and advanced users, \"Pro JavaScript with MooTools\" explores advanced features of the JavaScript language as well as the internals of MooTools 1.3.",
 		"cover": "/images/books/pro_javascript_with_mootools.jpg",
-		"alt_tags": "pro javascript, mootools, mark obcena",
+		"alt_tag": "Pro JavaScript with MooTools by Mark Obcena",
 		"buy_link": "http://www.amazon.com/Pro-JavaScript-MooTools-Mark-Obcena/dp/1430230541/"
 	}, {
 		"title": "MooTools Essentials",
 		"description": "The Official MooTools Reference for JavaScript and Ajax Development. Combined real-world examples with nuanced explanations of everything you need to know to make your site fun and engaging. <br />You’ll be adding effects and Ajax in no time.",
 		"cover": "/images/books/mootools_essentials.jpg",
-		"alt_tags": "mootools, aaron newton",
+		"alt_tag": "MooTools Essentials by Aaron Newton",
 		"buy_link": "http://www.amazon.com/MooTools-Essentials-JavaScript-Development-Firstpress/dp/1430209836"
 	}, {
 		"title": "MooTools 1.2 Beginner's Guide",
 		"description": "Learn how to create dynamic, interactive, and responsive cross-browser web applications using this popular JavaScript framework.<br />This book is perfect for MooTools newcomers. You do not require any familiarity with MooTools whatsoever, only a willingness to learn.",
 		"cover": "/images/books/mootools_1point2.jpg",
-		"alt_tags": "mootools, beginners, Jacob Gube, Garrick Cheung",
+		"alt_tag": "MooTools 1.2 Beginner's Guide by Jacob Gube and Garrick Cheung",
 		"buy_link": "https://www.packtpub.com/web-development/mootools-12-beginners-guide"
 	}
 ];
