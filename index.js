27a1602b5d0079e782bebdff451d453193dc2cc8
Sergio Crisostomo <sergiosbox@gmail.com>
2014-10-31 22:52:38 +0100

    review input fixes
    use `req.path != ‘/‘` instead of `req.path.length > 1`

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 // fix trailing slashes in path
 app.use(function(req, res, next){
-    if (req.path.substr(-1) == '/' && req.path.length > 1){
+    if (req.path != '/' && req.path.substr(-1) == '/'){
         var query = req.url.slice(req.path.length);
         res.redirect(301, req.path.slice(0, -1) + query);
     } else {
