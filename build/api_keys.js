b34392c6be39e1c5d5314d313cda9ec9ee66497c
Sergio Crisostomo <sergiosbox@gmail.com>
2015-07-14 00:22:03 +0200

    fix spaces to tabs

--- build/api_keys.js
+++ build/api_keys.js
@@ -6,36 +6,34 @@ var readline = require('readline');
 var rli;
 
 function checkKeys(site, done){
-    var keys = Object.keys(APIkeys[site]);
-    (function question() {
-        var key = keys.shift();
+	var keys = Object.keys(APIkeys[site]);
+	(function question(){
+		var key = keys.shift();
 		if (!key) return done();
-        rli.question('Please insert ' + site + '\'s ' + key + ': ', function(answer){
-            if (answer) APIkeys[site][key] = answer;
-            question();
-        });
-    })();
+		rli.question('Please insert ' + site + '\'s ' + key + ': ', function(answer){
+			if (answer) APIkeys[site][key] = answer;
+			question();
+		});
+	})();
 }
 
 function question(site, next){
-    rli.question('Do you want to add or change keys for ' + site + '? (y/n): ', function(answer){
-        if (answer.toLowerCase() == 'y') checkKeys(site, next);
-        else next();
-    });
+	rli.question('Do you want to add or change keys for ' + site + '? (y/n): ',
+		function(answer){
+			if (answer.toLowerCase() == 'y') checkKeys(site, next);
+			else next();
+		});
 }
-
 module.exports = function(){
 	var services = Object.keys(APIkeys);
 	rli = readline.createInterface({
-	    input: process.stdin,
-	    output: process.stdout
+		input: process.stdin,
+		output: process.stdout
 	});
-
 	(function iterator(){
-	    var service = services.shift();
-	    if (service) return question(service, iterator);
-
-	    rli.close();
+		var service = services.shift();
+		if (service) return question(service, iterator);
+		rli.close();
 		fs.writeFileSync(APIpath, JSON.stringify(APIkeys, null, 4));
 	})();
 }
